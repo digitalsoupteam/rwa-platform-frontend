ae98c609dedc926e3434191b5a532a8114987c36
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-08-15 15:49:33 +0400

    UI Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,63 @@
-import { Button } from '@/components/ui';
+import { Button, Title } from '@/components/ui';
 
 export default function Home() {
   return (
     <>
       <section className={'p-4'}>
         <div className={'bg-grey-dark p-4 text-white rounded-md'}>
-          Buttons
+          <Title className={'mb-2'} size={'xl'} level={1}>
+            Buttons
+          </Title>
           <div className={'flex flex-wrap gap-5'}>
             <div className={'flex flex-col gap-2 items-start'}>
-              <div>Primary button</div>
-              <Button visualType={'primary'}>Start trading</Button>
+              <Title className={'mb-2'} size={'md'} level={3}>
+                Primary
+              </Title>
+              <Button visualType={'primary'}>Primary normal</Button>
               <Button visualType={'primary'} disabled={true}>
                 Primary disabled
               </Button>
             </div>
             <div className={'flex flex-col gap-2 items-start'}>
-              <div>Secondary button</div>
+              <Title className={'mb-2'} size={'md'} level={3}>
+                Secondary
+              </Title>
               <Button visualType={'secondary'}>Secondary normal</Button>
               <Button visualType={'secondary'} disabled={true}>
                 Secondary disabled
               </Button>
             </div>
             <div className={'flex flex-col gap-2 items-start'}>
-              <div>Tertiary button</div>
+              <Title className={'mb-2'} size={'md'} level={3}>
+                Secondary
+              </Title>
               <Button visualType={'tertiary'}>Tertiary normal</Button>
               <Button visualType={'tertiary'} disabled={true}>
                 Tertiary disabled
               </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={'p-4'}>
+        <div className={'bg-grey-dark p-4 text-white rounded-md'}>
+          <Title className={'mb-2'} size={'xl'} level={1}>
+            Title
+          </Title>
+          <div className={'flex flex-wrap gap-5'}>
+            <div className={'flex flex-col gap-2 items-start'}>
+              <Title className={'mb-2'} size={'sm'} level={3}>
+                sm - H4 Heading
+              </Title>
+              <Title className={'mb-2'} size={'md'} level={3}>
+                md - H5/H3 Heading
+              </Title>
+              <Title className={'mb-2'} size={'lg'} level={3}>
+                lg - H3/H2 Heading
+              </Title>
+              <Title className={'mb-2'} size={'xl'} level={3}>
+                xl - H1 Heading
+              </Title>
             </div>
           </div>
         </div>
---
+++ src/components/ui/Title/Title.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import React, { FC, JSX, HTMLAttributes } from 'react';
+
+interface TitleProps extends HTMLAttributes<HTMLHeadingElement> {
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+  size: 'sm' | 'md' | 'lg' | 'xl';
+}
+
+const Title: FC<TitleProps> = ({ className, children, level = 1, size }) => {
+  const SIZES = {
+    sm: 'text-2xl leading-[1.2]',
+    md: 'text-xl tracking-[-0.02em] leading-[1.2] md:leading-none md:text-[2rem] md:tracking-tighter',
+    lg: 'text-[2rem] leading-none tracking-tighter md:text-[3.25rem] md:tracking-[-0.04em]',
+    xl: 'text-[2.875rem] tracking-[-0.04em] md:text-[4.625rem]',
+  };
+
+  const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+
+  return <Tag className={clsx(className, 'font-semibold', SIZES[size])}>{children}</Tag>;
+};
+
+export default Title;
