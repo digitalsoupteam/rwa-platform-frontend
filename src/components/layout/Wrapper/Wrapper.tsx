e87908e8a1830da76f6182a247ef5b626126a24f
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-09-03 14:59:01 +0400

    Layout Wrapper

---
+++ src/components/layout/Wrapper/Wrapper.tsx
@@ -0,0 +1,7 @@
+import React, { FC, PropsWithChildren } from 'react';
+
+const Wrapper: FC<PropsWithChildren> = ({ children }) => {
+  return <div className={'px-3 mx-auto min-w-[360px] max-w-[1480px] md:px-5'}>{children}</div>;
+};
+
+export default Wrapper;
