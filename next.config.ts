89ef031c8381085c4f9162a5e4cbf43523188032
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-09-04 20:30:22 +0400

    Export build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // distDir: 'build',
-  // images: {
-  //     unoptimized: true,
-  // },
+  output: 'export',
+  distDir: 'build',
+  images: {
+      unoptimized: true,
+  },
   trailingSlash: true,
   webpack: config => {
     config.module.rules.push({
